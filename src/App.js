3d64ccd9117d21707351b13e8edcea455f075c14
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-09-16 22:45:00 +0100

    Refactor code; add conditional rendering

--- src/App.js
+++ src/App.js
@@ -15,56 +15,83 @@ import ExerciseForm from "./components/ExerciseForm";
 function App() {
   const [collections, setCollections] = useState([]);
   const [exercises, setExercises] = useState([]);
-
+  /* This state shows if we are fetching data from the 
+  server, needed for application to work properly.*/
+  const [isLoadingCollections, setIsLoadingCollections] = useState(true);
+  const [isLoadingExercises, setIsLoadingExercises] = useState(true);
+  // Derived state to check if we currently fetching data from both endpoints.
+  const isLoading = isLoadingCollections || isLoadingExercises;
   console.log({ collections, exercises });
 
   useEffect(() => {
+    getCollections();
+  }, []);
+
+  useEffect(() => {
+    getExercises();
+  }, []);
+
+  function getCollections() {
+    setIsLoadingCollections(true);
     fetch("http://localhost:3030/collections")
       .then((res) => res.json())
       .then((collectionsData) => {
         console.log(collectionsData);
         setCollections(collectionsData);
+        setIsLoadingCollections(false);
       });
-  }, []);
+  }
 
-  useEffect(() => {
+  function getExercises() {
+    setIsLoadingExercises(true);
     fetch("http://localhost:3030/exercises")
       .then((res) => res.json())
       .then((exercisesData) => {
         console.log(exercisesData);
         setExercises(exercisesData);
+        setIsLoadingExercises(false);
       });
-  }, []);
+  }
 
   return (
     <>
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home collections={collections} />
-        </Route>
-        <Route exact path="/new-collection">
-          <CollectionForm />
-        </Route>
-        <Route exact path="/new-exercise">
-          <ExerciseForm />
-        </Route>
-        <Route exact path="/collections">
-          {collections && <Collections collections={collections} />}
-        </Route>
-        <Route exact path="/collections/:collectionTitle">
-          <Collection collections={collections} />
-        </Route>
-        <Route exact path="/exercises">
-          {exercises && <Exercises exercises={exercises} />}
-        </Route>
-        <Route exact path="/exercises/:exerciseId">
-          <Exercise exercises={exercises} />
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
+      {!isLoading && (
+        <Switch>
+          <Route exact path="/">
+            <Home collections={collections} />
+          </Route>
+          <Route exact path="/collections/new-collection">
+            <CollectionForm
+              collections={collections}
+              setCollections={setCollections}
+            />
+          </Route>
+          <Route exact path="/collections/:collectionTitle/new-exercise">
+            <ExerciseForm
+              exercises={exercises}
+              collections={collections}
+              setExercises={setExercises}
+              getCollections={getCollections}
+            />
+          </Route>
+          <Route exact path="/collections">
+            <Collections collections={collections} />
+          </Route>
+          <Route exact path="/collections/:collectionTitle">
+            <Collection collections={collections} />
+          </Route>
+          <Route exact path="/exercises">
+            <Exercises exercises={exercises} />
+          </Route>
+          <Route exact path="/exercises/:exerciseId">
+            <Exercise exercises={exercises} />
+          </Route>
+          <Route path="/not-found">
+            <NotFound />
+          </Route>
+        </Switch>
+      )}
     </>
   );
 }
