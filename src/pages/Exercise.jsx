0065c7d956a94442cb5ac93d601749e72c0bed5f
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-09-16 22:51:49 +0100

    Add validation and redirect to `NotFound` page

--- src/pages/Exercise.jsx
+++ src/pages/Exercise.jsx
@@ -1,15 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 export default function Exercise({ exercises }) {
   console.log("Inside Exercise: ", exercises);
 
   const { exerciseId } = useParams();
+  const history = useHistory();
+
+  // If exercise id is not a number, redirect to `404 Page Not Found`
+  if (!Number.isInteger(parseInt(exerciseId))) {
+    history.push("/not-found");
+  }
 
   const exercise = exercises.find(
     (exercise) => exercise.id === parseInt(exerciseId)
   );
 
+  // If exercise wasn't found, redirect to `404 Page Not Found`
   if (!exercise) {
+    history.push("/not-found");
     return null;
   }
 
