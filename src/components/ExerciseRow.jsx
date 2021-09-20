7b48d0a046285eaea4546babd50ba225265ab018
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-09-20 12:00:56 +0100

    Render `ChessBoard` component of each exercise

--- src/components/ExerciseRow.jsx
+++ src/components/ExerciseRow.jsx
@@ -1,14 +1,16 @@
 import { Button } from "@mui/material";
 import { useHistory } from "react-router-dom";
+import ChessBoard from "./ChessBoard";
 
 export default function ExerciseRow({ exercise }) {
   const history = useHistory();
-  const { id, difficulty } = exercise;
+  const { id, difficulty, FEN } = exercise;
 
   return (
     <li>
       <h2>Exercise #{id}</h2>
       <span>Difficulty: {difficulty}</span>
+      <ChessBoard position={FEN} />
 
       <Button
         onClick={() => history.push(`/exercises/${id}`)}
