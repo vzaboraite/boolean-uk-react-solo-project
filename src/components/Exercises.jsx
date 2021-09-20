209f708b9c881a6770ac94a47a98eb5a02bfd7a6
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-09-20 13:04:55 +0100

    Filter exercises by `difficulty`.

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ExerciseRow from "./ExerciseRow";
+import FilterByDifficultyForm from "./FilterByDifficultyForm";
 
 export default function Exercises({ exercises }) {
+  const [difficulty, setDifficulty] = useState("");
+
+  const handleDifficultyOptionsChange = (event) => {
+    setDifficulty(event.target.value);
+  };
+
+  function filterByDifficulty(exercises) {
+    if (difficulty === "") {
+      return exercises;
+    }
+
+    const filteredExercises = exercises.filter(
+      (exercise) => exercise.difficulty === difficulty
+    );
+    return filteredExercises;
+  }
+  const filteredExercises = filterByDifficulty(exercises);
+
   return (
-    <ul>
-      {exercises.map((exercise, index) => {
-        return <ExerciseRow exercise={exercise} key={index} />;
-      })}
-    </ul>
+    <>
+      <FilterByDifficultyForm
+        difficulty={difficulty}
+        onChange={handleDifficultyOptionsChange}
+      />
+      <ul>
+        {filteredExercises.map((exercise, index) => {
+          return <ExerciseRow exercise={exercise} key={index} />;
+        })}
+      </ul>
+    </>
   );
 }
---
+++ src/components/FilterByDifficultyForm.jsx
@@ -0,0 +1,18 @@
+export default function FilterByDifficultyForm({ difficulty, onChange }) {
+  return (
+    <form id="filter-by-type-form" autocompete="off">
+      <label htmlFor="filter-by-type"></label>
+      <select
+        name="filter-by-difficulty"
+        id="filter-by-difficulty"
+        value={difficulty}
+        onChange={onChange}
+      >
+        <option value="">Select a difficulty...</option>
+        <option value="easy">Easy</option>
+        <option value="medium">Medium</option>
+        <option value="hard">Hard</option>
+      </select>
+    </form>
+  );
+}
